c3cf53f7426f46b35893c57c3f0e049570fee853
徐成 <old_camel@163.com>
2022-06-07 15:49:47 +0800

    动态数据源build

--- core/trino-main/src/main/java/io/trino/metadata/StaticCatalogStore.java
+++ core/trino-main/src/main/java/io/trino/metadata/StaticCatalogStore.java
@@ -47,9 +47,6 @@ import java.nio.charset.StandardCharsets;
 import java.security.spec.AlgorithmParameterSpec;
 
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
